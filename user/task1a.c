e340871b1a3517831da233a8710a58cde2a52a72
Maxim <maxim@MacBook-Air-Maxim.local>
2025-10-25 11:41:00 -0700

    1 iteration

---
+++ user/task1a.c
@@ -0,0 +1,29 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+
+int
+main(int argc, char **argv)
+{
+  int pid = fork();
+  if(pid < 0){
+    fprintf(2, "fork failed\n");
+    exit(1);
+  }
+  if(pid == 0){
+    sleep(50);
+    exit(1);
+  } else {
+    int status = 0;
+    int w = wait(&status);
+    if(w < 0){
+      fprintf(2, "wait failed\n");
+      exit(1);
+    }
+    printf("child pid=%d, exit status=%d\n", w, status);
+    exit(0);
+  }
+}
+
+
